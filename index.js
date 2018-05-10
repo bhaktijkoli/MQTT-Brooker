1e1616c2e0daa5513e42856fc7024d48fb2ae1ab
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-05-10 13:35:30 +0530

    MQTT Auth

--- index.js
+++ index.js
@@ -7,19 +7,44 @@ var settings = {
 var httpServ = http.createServer()
 var server = new mosca.Server(settings);
 server.attachHttpServer(httpServ);
+httpServ.listen(3000, ()=>console.log("Web socket server is running."));
+
+// Accepts the connection if the username and password are valid
+var authenticate = function(client, username, password, callback) {
+  var authorized = (username === 'admin' && password.toString() === 'root');
+  if (authorized) client.user = username;
+  callback(null, authorized);
+}
+
+// In this case the client authorized as alice can publish to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+var authorizePublish = function(client, topic, payload, callback) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+// In this case the client authorized as alice can subscribe to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+var authorizeSubscribe = function(client, topic, callback) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+
 
 server.on('clientConnected', function(client) {
-    console.log('client connected', client.id);
+  console.log('client connected', client.id);
 });
 
 // fired when a message is received
 server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
+  console.log('Published', packet.payload.toString());
 });
 
 server.on('ready', setup);
 
 // fired when the mqtt server is ready
 function setup() {
-  console.log('Mosca server is up and running');
+  server.authenticate = authenticate;
+  server.authorizePublish = authorizePublish;
+  server.authorizeSubscribe = authorizeSubscribe;
+  console.log('Mosca server is up and running.');
 }
